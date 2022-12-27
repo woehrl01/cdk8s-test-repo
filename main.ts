f91dd573f7efa86386663e7c740143c866c422bc
Lukas Wöhrl <lukas@woehrl.net>
2022-12-27 15:55:26 +0000

    use correct prefix

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ import { Synthesizer } from './synthesizer';
 import ProjectA from "./ProjectA";
 import RootChart from "./RootChart";
 
-const chart = process.env.CHART || "";
-const clustername = process.env.CLUSTER_NAME || "";
-const clusterenvironment = process.env.CLUSTER_ENVIRONMENT || ""
+const chart = process.env.CHART || process.env.ARGOCD_ENV_CHART || "";
+const clustername = process.env.CLUSTER_NAME || process.env.ARGOCD_ENV_CLUSTER_NAME || "";
+const clusterenvironment = process.env.CLUSTER_ENVIRONMENT || process.env.ARGOCD_ENV_CLUSTER_ENVIRONMENT || ""
 
 const synth = new Synthesizer();
 
